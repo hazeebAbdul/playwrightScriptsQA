4f137277ea077432aa69f969e3488f4f2ae9e33b
Haseeb Abdul <haseeb@Haseebs-MacBook-Air.local>
2025-06-25 22:51:18 -0400

    added two pytest files

---
+++ test_mocktest.py
@@ -0,0 +1,9 @@
+def test_multiply():
+    assert 3 * 2 == 6
+
+
+def test_addition():
+    assert 2 + 2 == 4
+
+def test_subtraction():
+    assert 5 - 2 == 4
---
+++ test_mocktest2.py
@@ -0,0 +1,9 @@
+def test_multiply():
+    assert 3 * 3 == 9
+
+
+def test_addition():
+    assert 2 + 3 == 1
+
+def test_subtraction():
+    assert 5 - 2 == 4
